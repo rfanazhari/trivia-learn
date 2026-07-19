379a548eba39fae5cd5ebc5450ac656e76749f49
rfanazhari <afz60.30@gmail.com>
2026-07-19 18:28:48 +0700

    feat: GREEN Implement accessor methods and enhance OwnerSnapshot initialization
    - Added `FirstName` and `LastName` accessor methods to `OwnerSnapshot`.
    - Updated `NewOwnerSnapshot` constructor to initialize `OwnerSnapshot` fields with `PersonName` values.

--- internal/account/owner_snapshot_vo.go
+++ internal/account/owner_snapshot_vo.go
@@ -3,8 +3,21 @@ package account
 import "learn-trivia/internal/customer"
 
 type OwnerSnapshot struct {
+	firstName string
+	lastName  string
 }
 
-func NewOwnerSnapshot(name customer.PersonName) *OwnerSnapshot {
-	return &OwnerSnapshot{}
+func NewOwnerSnapshot(name customer.PersonName) OwnerSnapshot {
+	return OwnerSnapshot{
+		firstName: name.FirstName(),
+		lastName:  name.LastName(),
+	}
+}
+
+func (s OwnerSnapshot) FirstName() string {
+	return s.firstName
+}
+
+func (s OwnerSnapshot) LastName() string {
+	return s.lastName
 }
